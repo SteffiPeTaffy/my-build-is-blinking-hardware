753a11ef574c37e3474ddf37134572e1d7e77cbe
Stefanie Grewenig <sgreweni@thoughtworks.com>
2016-02-18 17:11:07 +0100

    initial commit

---
+++ src/hotspot.lua
@@ -0,0 +1,51 @@
+local moduleName = ... 
+local M = {}
+_G[moduleName] = M
+
+function M.blinkLED(color)
+    ws2812.writergb(4, color)
+    tmr.alarm(6, 500, 0, function() 
+        ws2812.writergb(4, string.char(0,0,16))
+    end )
+end
+
+function M.getHTMLPage(pageName)
+  local page = ""
+  file.open(pageName, "r")
+  text = file.read("\n")
+  while (text ~= nil) do
+    text = text:gsub("$HEAP", node.heap())
+    page = page .. text
+    text = file.read()
+  end  
+  file.close()  
+  return page
+end
+
+function M.startHotspot() 
+    print("Starting hotspot")
+    local cfg={}
+    cfg.ssid="ESP Test AP"
+    cfg.pwd="password"
+    
+    wifi.setmode(wifi.SOFTAP)
+    wifi.ap.config(cfg)
+end
+
+function M.startServer(timeout)
+    print("Starting server")
+    srv=net.createServer(net.TCP, timeout)
+    srv:listen(80,function(conn)
+      conn:on("receive",function(conn,payload)
+        M.blinkLED(string.char(15,127,15))
+        print(payload)
+        htmlString = M.getHTMLPage("index.html")
+        conn:send(htmlString)
+      end)
+      conn:on("sent",function(conn)
+        conn:close()
+      end)
+    end)
+end
+
+return M
---
+++ src/init.lua
@@ -0,0 +1,3 @@
+local hs = require("hotspot")
+hs.startHotspot()
+hs.startServer(10)
