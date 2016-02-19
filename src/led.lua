7a11ef46e92afe5b3afb9c0efe2527deb2c75fba
Philip Peter <github@justgeek.de>
2016-02-19 17:16:01 +0100

    Extracted led blinking in separate module

---
+++ src/led.lua
@@ -0,0 +1,15 @@
+local moduleName = ...
+local M = {}
+_G[moduleName] = M
+
+M.ledTimer = 6
+M.ledPin = 4
+
+function M.blinkLED(startColor, endColor, duration)
+    ws2812.writergb(M.ledPin, startColor)
+    tmr.alarm(M.ledTimer, duration, 0, function()
+        ws2812.writergb(M.ledPin, endColor)
+    end )
+end
+
+return M
